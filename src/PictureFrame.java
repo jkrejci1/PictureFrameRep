4d16b4c7d2483bdf053ac795b832e69d7c941ba5
Jack Krejci <jkrejci1988@gmail.com>
2021-11-22 16:40:53 -0600

    The imports to the picture frame class.

--- src/PictureFrame.java
+++ src/PictureFrame.java
@@ -1,4 +1,19 @@
+//Imports
+import java.awt.BorderLayout;
 
-public class PictureFrame {
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//CAN USE CTRL-->SHIFT-->O to automatically put missing inputs
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class PictureFrame extends JFrame {
 
 }
