c3136faef5b395ca3813f8075e92d9d998bbb2d8
Jack Krejci <jkrejci1988@gmail.com>
2021-11-22 17:16:22 -0600

    Created the variable for the text file being used, called the PictureDataReader's readPictureFromFile function to be created to be able to use the data needed, and made the call needed for the picture frame in the App class.

--- src/App.java
+++ src/App.java
@@ -6,6 +6,14 @@
  */
 public class App {
 	public static void main(String[] args) {
+		//Declare and initialize the variable for the text file
+		String fname = "descriptions.txt";
+		
+		//Used to read the text file being used to the program for our creation
+		PictureDataReader.readPictureDataFromFile(fname);
+		
+		//Used to create the frame of the app
+		PictureFrame pf = new PictureFrame();
 		
 	}
 }
