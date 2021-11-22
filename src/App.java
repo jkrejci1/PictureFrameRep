41a37c20d1f3ccf2f673540cedb6d13993417f87
Jack Krejci <jkrejci1988@gmail.com>
2021-11-22 17:09:12 -0600

    JavaDoc comment for the main class (App).

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
 
+/**
+ * Main Class - Used for the creation of everything, also used for the reading of the file for PictureData
+ * @author Jack
+ *
+ */
 public class App {
 	public static void main(String[] args) {
 		
