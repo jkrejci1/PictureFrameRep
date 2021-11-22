c65213092d8093cef81097fd25644fcdc89aa203
Jack Krejci <jkrejci1988@gmail.com>
2021-11-22 16:36:30 -0600

    Changes made for imports and the creation of the PicturePanel class.

---
+++ src/PicturePanel.java
@@ -0,0 +1,19 @@
+//Imports
+import java.awt.BorderLayout;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//CAN USE CTRL-->SHIFT-->O to automatically put missing inputs
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class PicturePanel extends JPanel{
+
+}
