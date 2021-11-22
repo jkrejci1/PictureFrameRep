d9f8aaf9f376406cfe0360a166da4d01c5c56154
Jack Krejci <jkrejci1988@gmail.com>
2021-11-22 16:52:12 -0600

    Added the getters/setters and private data members to the PictureData class.

--- src/PictureData.java
+++ src/PictureData.java
@@ -5,5 +5,37 @@
  *
  */
 public class PictureData {
-
+	private String fileName;
+	private String date;
+	private String description;
+	public String getFileName() {
+		return fileName;
+	}
+	
+	//Getters and Setters
+	
+	//Getter/Setter for fileName
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	//Getter/Setter for date
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	//Getter/Setter for description
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 }
