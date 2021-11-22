47aa6c9d56f155ecde03c1e09949dca1c619da50
Jack Krejci <jkrejci1988@gmail.com>
2021-11-22 17:19:56 -0600

    Created the function to read the picture data from the text file given from App in the PictureDataReader class. Also added a JavaDoc comment for the function.

--- src/PictureDataReader.java
+++ src/PictureDataReader.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
 
 public class PictureDataReader {
-
+	
+	/**
+	 * The function for creating an array list of our images and their data.
+	 * @param fname - Name of the text file
+	 * @return - The ArrayList 
+	 */
+	public static ArrayList<PictureData> readPictureDataFromFile(String fname) {
+		
+	}
 }
