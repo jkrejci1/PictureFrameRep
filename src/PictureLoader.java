ea64d30425c8528e9a10913bdf071d396cfec238
Jack Krejci <jkrejci1988@gmail.com>
2021-11-22 18:19:45 -0600

    Created the function for loading the images into memory.

--- src/PictureLoader.java
+++ src/PictureLoader.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
-
+import java.awt.image.BufferedImage;;
 /**
  * This class is used to create an array list of our pictures for the application.
  * @author Jack
  *
  */
 public class PictureLoader {
-
+	
+	public static ArrayList<BufferedImage> loadImagesFromPictureData(PictureData imgData) {
+		//Create an array list for the images themselves to be loaded in
+		ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
+		
+		return images;
+	}
 }
