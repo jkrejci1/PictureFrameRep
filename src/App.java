75381b3112e01a8bfac2f7ef860f30fc9558ac39
Jack Krejci <jkrejci1988@gmail.com>
2021-11-22 18:38:53 -0600

    Added a check in the App class which will be commeneted out after use to check if the images were loaded correctly to the buffered images class. Finished the loader class the load all the images to a buffered image array.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.awt.image.BufferedImage;
 
 /**
  * Main Class - Used for the creation of everything, also used for the reading of the file for PictureData
@@ -13,11 +14,13 @@ public class App {
 		//Used to read the text file being used to the program for our creation
 		ArrayList<PictureData> imgData = PictureDataReader.readPictureDataFromFile(fname);
 		
+		//Call the PictureLoader class to create list of the buffered images
+		ArrayList<BufferedImage> theImages = PictureLoader.loadImagesFromPictureData(imgData);
 		//Used to create the frame of the app
 		PictureFrame pf = new PictureFrame();
 		pf.setVisible(true);
 		
 		//Keep check if the imgData array has processed correctly or not
-		//System.out.println(imgData);
+		System.out.println(theImages);
 	}
 }
--- src/PictureLoader.java
+++ src/PictureLoader.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
-import java.awt.image.BufferedImage;;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 /**
  * This class is used to create an array list of our pictures for the application.
  * @author Jack
@@ -7,10 +11,24 @@ import java.awt.image.BufferedImage;;
  */
 public class PictureLoader {
 	
-	public static ArrayList<BufferedImage> loadImagesFromPictureData(PictureData imgData) {
+	public static ArrayList<BufferedImage> loadImagesFromPictureData(ArrayList<PictureData> imgData) {
+		BufferedImage imageFile = null;
+		String imageFromFile;
+	
 		//Create an array list for the images themselves to be loaded in
 		ArrayList<BufferedImage> images = new ArrayList<BufferedImage>();
 		
+		//Use the picture data to created a Buffered Image array of the images
+		for (PictureData i : imgData) {
+			imageFromFile = i.getPicFile();
+			try {
+				imageFile = ImageIO.read(new File(imageFromFile));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			images.add(imageFile);
+		}
 		return images;
 	}
 }
