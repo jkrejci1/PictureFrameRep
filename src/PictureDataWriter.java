da92f2168e10ec1663fd1cd2b23f8ca1a5acc49b
Jack Krejci <jkrejci1988@gmail.com>
2021-11-24 12:26:56 -0600

    Updated the writer class to not try and read and write onto the file at the same time, but instead just go through the array of image data and write it properly onto the text file.

--- src/PictureDataWriter.java
+++ src/PictureDataWriter.java
@@ -20,28 +20,32 @@ public class PictureDataWriter {
 		String fname = "descriptions.txt";
 		String line;
 		String[] parts;
-		int n = 0; //Keeps track of specific line we're on tied to a specific index
 		//The array given by the PictureFrame class contains all the data/new data for the description and date of each image frame
 		try {
-			Scanner fsc = new Scanner(new File(fname));
 			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(new File("test2.txt"))));
-			
-			while (fsc.hasNextLine()) {
-				line = fsc.nextLine();
-				parts = line.split("\t");
-				
-				//Overwrite each line with the current data in the text file
-				
-				//For ALL pictures
-				if (parts[0].equals(imgData.get(n).getPicFile())) {
-					pw.print(imgData.get(n).getPicFile()+"\t");
-					pw.print(imgData.get(n).getDate()+"\t");
-					pw.print(imgData.get(n).getDescription()+"\n");
-				}
-				//Add one as the line will go to the next line meaning the next index
-				n += 1;
+			for (PictureData i : imgData) {
+				pw.print(i.getPicFile()+"\t");
+				pw.print(i.getDate()+"\t");
+				pw.print(i.getDescription()+"\n");
 			}
-			fsc.close();
+			
+			//PREVIOUS WAY OF DOING IT IGNORE THIS!
+//			while (fsc.hasNextLine()) {
+//				line = fsc.nextLine();
+//				parts = line.split("\t");
+//				
+//				//Overwrite each line with the current data in the text file
+//				
+//				//For ALL pictures
+//				if (parts[0].equals(imgData.get(n).getPicFile())) {
+//					pw.print(imgData.get(n).getPicFile()+"\t");
+//					pw.print(imgData.get(n).getDate()+"\t");
+//					pw.print(imgData.get(n).getDescription()+"\n");
+//				}
+				//Add one as the line will go to the next line meaning the next index
+				
+			//}
+			
 			pw.close();
 		} catch (Exception ex) {
 			System.out.println("Failed to overwrite file!");
